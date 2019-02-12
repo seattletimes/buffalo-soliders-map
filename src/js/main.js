7a4d9808f78711be9ea63d96aaecb08797807e55
emilymeng <eng.emilym@gmail.com>
2019-02-12 15:19:27 -0800

    project parts

--- src/js/main.js
+++ src/js/main.js
@@ -7,13 +7,133 @@ var element = document.querySelector("leaflet-map");
 var L = element.leaflet;
 var map = element.map;
 
-//ICH code for popup template if needed----------
-// var ich = require("icanhaz");
-// var templateFile = require("./_popup.html");
-// ich.addTemplate("popup", templateFile);
+var $ = require("jquery");
 
-// var onEachFeature = function(feature, layer) {
-//   layer.bindPopup(ich.popup(feature.properties))
-// };
+var data = require("./buffalo.geo.json");
+data.features.sort((a, b) => a.properties.order - b.properties.order);
+
+//ICH code for popup template if needed----------
+var ich = require("icanhaz");
+var templateFile = require("./_popup.html");
+ich.addTemplate("popup", templateFile);
+
+function commafy( num ) {
+ num = num.toString();
+ if (!num) return;
+ if (num.length >= 4) {
+   num = num.replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+ }
+ return num;
+}
+
+
+function highlightFeature(e) {
+    var layer = e.target;
+    layer.setStyle({
+      radius: 10,
+      fillColor: "rgb(60, 60, 60)",
+      color: "#ffffff",
+      weight: 2,
+      opacity: .5,
+      fillOpacity:1
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+}
+function resetHighlight(e) {
+
+    geojson.resetStyle(e.target);
+}
+
+var currentItem = null;
+
+var onEachFeature = function(feature, layer) {
+  feature.layer = layer;
+
+  layer.on('click', function(e) {
+    currentItem = feature;
+    var index = data.features.indexOf(feature);
+    $(".goto.back").toggleClass("disabled", index == 0);
+    $(".goto.next").toggleClass("disabled", index == data.features.length - 1);
+    map.fitBounds(e.target.getBounds(), {maxZoom: 11});
+    var container = $(".pop-container");
+    container.html(ich.popup(feature.properties));
+    $(".close").click(function() {
+      container.html("");
+    });
+  });
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight
+  })
+
+};
 
  map.scrollWheelZoom.disable();
+
+// function getColor(d) {
+//   return d == "OIS" ? "#f48c38" :
+//                        "#446ba4"
+// }
+
+function geojsonMarkerOptions(feature) {
+
+  return {
+    radius: 10,
+    // fillColor: getColor(feature.properties.type),
+    fillColor: "#cc5500",
+    color: "#ffffff",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.6
+  }
+};
+
+var geojson = L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng);
+    },
+    style: geojsonMarkerOptions,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+
+$(".goto").on("click", function() {
+  if (currentItem === null) {
+    data.features[0].layer.fire("click");
+    return;
+  }
+  var shift = this.classList.contains("next") ? 1 : -1;
+  var index = data.features.indexOf(currentItem);
+  geojson.resetStyle(data.features[index].layer);
+  index += shift;
+
+  if (index < 0) return;
+  if (index >= data.features.length) return;
+
+  data.features[index].layer.fire("click");
+  data.features[index].layer.setStyle({
+    radius: 10,
+    fillColor: "#cc5500",
+    color: "#ffffff",
+    weight: 2,
+    opacity: 1,
+    fillOpacity:.6
+  });
+
+});
+//
+// $(".goto").on("mouseout", function() {
+//   if (currentItem === null) {
+//     data.features[0].layer.fire("click");
+//     return;
+//   }
+//   var shift = this.classList.contains("next") ? 1 : -1;
+//   var index = data.features.indexOf(currentItem);
+//   geojson.resetStyle(data.features[index].layer);
+// });
+
+
+map.fitBounds(geojson.getBounds());
